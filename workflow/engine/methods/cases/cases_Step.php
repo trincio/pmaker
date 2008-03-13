2683038c59496d668ef999a62dd7160320948038
ferontiveros <trincio@users.noreply.github.com>
2008-03-13 23:13:31 +0000

    juliocesar: Los campos de tipo listox ya pueden ser dependientes, adem?\195?\161s de otros cambios menores

--- workflow/engine/methods/cases/cases_Step.php
+++ workflow/engine/methods/cases/cases_Step.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * cases_Step.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
   /* Permissions */
@@ -228,7 +228,7 @@ switch ($_GET['TYPE'])
           }
         }
         $sFilename = G::replaceDataField($aOD['OUT_DOC_FILENAME'], $aApplication['APP_DATA']);*/
-        $sFilename = G::replaceDataField($aOD['OUT_DOC_FILENAME'], $Fields['APP_DATA']);
+        $sFilename = ereg_replace('[^A-Za-z0-9_]', '_', G::replaceDataField($aOD['OUT_DOC_FILENAME'], $Fields['APP_DATA']));
         //$oOutputDocument->generate($_GET['UID'], $aApplication['APP_DATA'], PATH_DOCUMENT . $_SESSION['APPLICATION'] . '/outdocs/', $sFilename, $aOD['OUT_DOC_TEMPLATE']);
         $oOutputDocument->generate($_GET['UID'], $Fields['APP_DATA'], PATH_DOCUMENT . $_SESSION['APPLICATION'] . '/outdocs/', $sFilename, $aOD['OUT_DOC_TEMPLATE']);
         require_once 'classes/model/AppDocument.php';
