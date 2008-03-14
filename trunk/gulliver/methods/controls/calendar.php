4c6b111bbf4ab6faaf1fe0d9ecfd44c3fb2b2ff8
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-03-14 17:04:51 +0000

    juliocesar: Bugs 0002203  y 0002210 resueltos

--- trunk/gulliver/methods/controls/calendar.php
+++ trunk/gulliver/methods/controls/calendar.php
@@ -159,7 +159,7 @@
 			if ( $strDate < $dtmin_value || $strDate > $dtmax_value)
 				$link = "$strDay";
 			else
-				$link = "<a href='#' onclick=\"selectDate('$strDate');return false;\" >$strDay</a>";
+				$link = "<a href='#' onclick=\"selectDate('$strDate');eval('if (form_' + datePickerPanel.formId + '.getElementByName(datePickerPanel.idName)) {form_' + datePickerPanel.formId + '.getElementByName(datePickerPanel.idName).updateDepententFields();}');return false;\" >$strDay</a>";
 			print "<td bgcolor='$bgcolor' align='center' width='14%'>$link</td>";
 		}
 		print "</tr>";
