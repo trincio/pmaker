973bc29c544d213d08d16703893d1d786ed43441
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-03-18 16:36:16 +0000

    juliocesar: Bugs encontrados corriendo en windows, corregidos

--- trunk/gulliver/system/class.g.php
+++ trunk/gulliver/system/class.g.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * class.g.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
 /**
@@ -702,9 +702,10 @@ class G
         case 'js' :
           G::sendHeaders ( $filename , 'text/javascript', $download, $downloadFileName ); break;
         case 'htm' :
-          G::sendHeaders ( $filename , 'text/html', $download, $downloadFileName ); break;
         case 'html' :
           G::sendHeaders ( $filename , 'text/html', $download, $downloadFileName ); break;
+        case 'json' :
+          G::sendHeaders ( $filename , 'text/plain', $download, $downloadFileName ); break;
         case 'gif' :
           G::sendHeaders ( $filename , 'image/gif', $download, $downloadFileName ); break;
         case 'png' :
@@ -1641,10 +1642,10 @@ class G
   		$car = strtolower ( $text[$i] );
   		if ( strpos( "abcdefghijklmnopqrstuvwxyz1234567890", $car ) !== false ) {
   			if ($space ) $car = strtoupper ( $car );
-  		  $result .= $car; 
+  		  $result .= $car;
   		  $space  = false;
   		}
-  		else 
+  		else
   		  $space = true;
   	}
     return $result;
