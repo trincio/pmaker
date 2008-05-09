53098e7c0ad4e509861e15e7fb7a4e514d396490
ferontiveros <trincio@users.noreply.github.com>
2008-05-09 01:07:30 +0000

    erik: the data base servers adds interface was added but just in the first part with a test port, test service, and test connection

---
+++ workflow/engine/classes/class.net.php
@@ -0,0 +1,198 @@
+<?php
+/*--------------------------------------------------
+| net.class.php
+| By Erik Amaru Ortiz
+| CopyLeft (f) 2008 
+| Email: erik@colosa.com
++--------------------------------------------------
+| Email bugs/suggestions to erik@colosa.com erik.260mb.com
++--------------------------------------------------
+| This script has been created and released under
+| the GNU GPL and is free to use and redistribute
+| only if this copyright statement is not removed
++--------------------------------------------------*/
+
+class NET
+{
+    public $hostname;
+    public $ip;
+
+    /*vars for the errors handle*/
+    private $errno;
+    private $errstr;
+
+    function __construct($pHost)
+    {
+        $this->errno = 0;
+        $this->errstr = "";
+
+        #verifing valid param
+        if ($pHost == "") {
+            $this->errno = 1000;
+            $this->errstr = "NET::You must specify a host";
+            //$this->showMsg();
+        }
+        $this->resolv($pHost);
+    }
+
+    function resolv($pHost)
+    {
+        if ($this->is_ipaddress($pHost)) {
+            $this->ip = $pHost;
+            if (!$this->hostname = @gethostbyaddr($pHost)) {
+                $this->errno = 2000;
+                $this->errstr = "NET::Host down";
+            }
+        } else {
+            if (!$this->ip = @gethostbyname($pHost)) {
+                $this->errno = 2000;
+                $this->errstr = "NET::Host down";
+            }
+            $this->hostname = $pHost;
+        }
+    }
+
+
+    function scannPort($pPort)
+    {
+        define('TIMEOUT', 5);
+        $hostip = @gethostbyname($host); // resloves IP from Hostname returns hostname on failure
+        // attempt to connect
+        if (@fsockopen($this->ip, $pPort, $this->errno, $this->errstr, TIMEOUT)) {
+            return true;
+            @fclose($x); //close connection (i dont know if this is needed or not).
+        } else {
+            return false;
+        }
+
+    }
+
+    function is_ipaddress($pHost)
+    {
+        $key = true;
+        #verifing if is a ip address
+        $tmp = explode(".", $pHost);
+        #if have a ip address format
+        if (count($tmp) == 4) {
+            #if a correct ip address
+            for ($i = 0; $i < count($tmp); $i++) {
+                if (!is_int($tmp[$i])) {
+                    $key = false;
+                    break;
+                }
+            }
+        } else {
+            $key = false;
+        }
+        return $key;
+    }
+
+    function ping($pTTL = 3000)
+    {
+        //$output=shell_exec($comm);
+        //echo "<pre>".$output."</pre>";
+        echo $cmd = "ping -w 3000 $this->ip";
+        $output = exec($cmd, $a, $a1);
+        $this->errstr = "";
+        for ($i = 0; $i < count($a); $i++)
+            $this->errstr += $a[$i];
+        $this->errno = $a1;
+    }
+
+    function mysql_ping($pUser, $pPasswd)
+    {
+        if ($pPasswd != "") {
+            $link = @mysql_connect($this->ip, $pUser, $pPasswd);
+        } else {
+            $link = @mysql_connect($this->ip, $pUser, "");
+        }
+        if ($link) {
+            if (@mysql_ping($link)) {
+                $this->errstr = "";
+                $this->errno = 0;
+            } else {
+                $this->errstr = "NET::MYSQL->Lost Connection";
+                $this->errno = 10010;
+            }
+        } else {
+            $this->errstr = "NET::MYSQL->The connection was refused";
+            $this->errno = 10001;
+        }
+        return;
+    }
+
+    function mysql_testDataBase($pUser, $pPasswd, $pDb)
+    {
+        set_time_limit(0);
+        $link = @mysql_connect($this->ip, $pUser, $pPasswd);
+        $db = @mysql_select_db($pDb);
+        if ($link) {
+            if ($db) {
+                $result = @mysql_query("show tables;");
+                if ($result) {
+                    $this->errstr = "";
+                    $this->errno = 0;
+                    @mysql_free_result($result);
+                } else {
+                    $this->errstr = "NET::MYSQL->Test query failed";
+                    $this->errno = 10100;
+                }
+            } else {
+                $this->errstr = "NET::MYSQL->Select data base failed";
+                $this->errno = 10011;
+            }
+        } else {
+            $this->errstr = "NET::MYSQL->The connection was refused";
+            $this->errno = 10001;
+        }
+    }
+
+    function pg_ping($pUser, $pPasswd, $pDb, $pPort)
+    {
+        $pPort = ($pPort == "") ? "5432" : $pPort;
+        $link = pg_pconnect("host='$this->ip' port='$pPort' user='$pUser' password='$pPasswd' dbname='$pDb' ");
+        if ($link) {
+            if (pg_ping($link)) {
+                $this->errstr = "";
+                $this->errno = 0;
+            } else {
+                $this->errstr = "NET::POSTGRES->Lost Connection";
+                $this->errno = 20010;
+            }
+        } else {
+            $this->errstr = "NET::POSTGRES->The connection was refused";
+            $this->errno = 20001;
+        }
+    }
+
+    function showMsg()
+    {
+        if ($this->errno != 0) {
+            $msg = "
+			<center>
+			<fieldset style='width:90%'><legend>Class NET</legend>
+			  <div align=left>	
+				<font color='red'>
+					<b>NET::ERROR NO -> $this->errno<br/>
+					NET::ERROR MSG -> $this->errstr</b>
+				</font>
+			  </div>	
+			</fieldset>
+			<center>";
+            print ($msg);
+        }
+    }
+
+    function getErrno()
+    {
+        return $this->errno;
+    }
+
+    function getErrmsg()
+    {
+        return $this->errstr;
+    }
+
+}
+
+?>
--- workflow/engine/methods/setup/dbConnectionsAjax.php
+++ workflow/engine/methods/setup/dbConnectionsAjax.php
@@ -23,26 +23,114 @@
  *
  */
 switch ($_POST['action']) {
-  case 'showConnections':
-    require_once 'classes/model/DbSource.php';
-    $oDBSource = new DbSource();
-    $oCriteria = $oDBSource->getCriteriaDBSList($_POST['PRO_UID']);
-    global $G_PUBLISH;
-    $G_PUBLISH = new Publisher();
-    $G_PUBLISH->AddContent('propeltable', 'paged-table', 'setup/dbConnections', $oCriteria);
-    $G_HEADER->clearScripts();
-    G::RenderPage('publish', 'raw');
-  break;
-  case 'newOrEditDBConnection':
-    if ($_POST['DBS_UID'] != '') {
-      $aFields = $_POST;
-    }
-    else {
-      $aFields = $_POST;
-    }
-    $G_PUBLISH = new Publisher();
-    $G_PUBLISH->AddContent('xmlform', 'xmlform', 'setup/dbConnections_Edit', '', $aFields);
-    $G_HEADER->clearScripts();
-    G::RenderPage('publish', 'raw');
-  break;
+    case 'showConnections':
+        require_once 'classes/model/DbSource.php';
+        $oDBSource = new DbSource();
+        $oCriteria = $oDBSource->getCriteriaDBSList($_POST['PRO_UID']);
+        global $G_PUBLISH;
+        $G_PUBLISH = new Publisher();
+        $G_PUBLISH->AddContent('propeltable', 'paged-table', 'setup/dbConnections', $oCriteria);
+        $G_HEADER->clearScripts();
+        G::RenderPage('publish', 'raw');
+        break;
+    case 'newOrEditDBConnection':
+        if ($_POST['DBS_UID'] != '') {
+            $aFields = $_POST;
+        } else {
+            $aFields = $_POST;
+        }
+        $G_PUBLISH = new Publisher();
+        $G_PUBLISH->AddContent('xmlform', 'xmlform', 'setup/dbConnections_Edit', '', $aFields);
+        $G_HEADER->clearScripts();
+        G::RenderPage('publish', 'raw');
+        break;
+
+    case 'saveConnection':
+    	
+    	$pro_uid = $_POST['pro_uid'];
+        $type = $_POST['type'];
+        $server = $_POST['server'];
+        $db_name = $_POST['db_name'];
+        $user = $_POST['user'];
+        $passwd = ($_POST['passwd'] == 'none') ? "":$_POST['passwd'];
+        $port = ($_POST['port'] == 'none') ? "":$_POST['port'];
+        
+        require_once 'classes/model/DbSource.php';
+        $oDBSource = new DbSource();
+		//$oDBSource->setDbsUid();
+		$oDBSource->setProUid($pro_uid);
+		$oDBSource->setDbsType($type);
+		$oDBSource->setDbsServer($server);
+		$oDBSource->setDbsDatabaseName($db_name);
+		$oDBSource->setDbsUsername($user);
+		$oDBSource->setDbsPassword($passwd);
+		$oDBSource->setDbsPort($port);
+		 
+		$oDBSource->save();
+		echo "<center><font color=blue><b>The Server configuration wass added successfully!</b><font></center>";    
+        break;
+
+    case 'showTestConnection':
+
+        $G_PUBLISH = new Publisher;
+        $G_PUBLISH->AddContent('view', 'setup/dbConnections');
+        G::RenderPage('publish', 'raw');
+        break;
+
+    case 'testConnection':
+        sleep(0);
+        $teststep = $_POST['teststep'];
+        $type = $_POST['type'];
+        $server = $_POST['server'];
+        $db_name = $_POST['db_name'];
+        $user = $_POST['user'];
+        $passwd = ($_POST['passwd'] == 'none') ? "":$_POST['passwd'];
+        $port = ($_POST['port'] == 'none') ? "":$_POST['port'];
+
+        require_once 'classes/class.net.php';
+        G::LoadClass('NET');
+        $db_server = new NET($server);
+
+        switch ($teststep) {
+            case 'resolv':
+                if ($db_server->getErrno() == 0) {
+                    print ('true');
+                } else {
+                    print ('false');
+                }
+                break;
+
+            case 'checkport':
+                $db_server->scannPort($port);
+                if ($db_server->getErrno() == 0) {
+                    print ('true');
+                } else {
+                    print ('false');
+                }
+                break;
+
+            case 'mysql_ping':
+                $db_server->mysql_ping($user, $passwd);
+                if ($db_server->getErrno() == 0) {
+                    print ('true');
+                } else {
+                    print ('false');
+                }
+                break;
+
+            case 'mysql_testDataBase':
+                $db_server->mysql_testDataBase($user, $passwd, $db_name);
+                if ($db_server->getErrno() == 0) {
+                    print ('true');
+                } else {
+                    print ('false');
+                }
+                break;
+
+            default:
+                echo 'default';
+        }
+
+
+        break;
 }
