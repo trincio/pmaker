7430d79017bec5d5c18524bde0b2dbed2ced2115
ferontiveros <trincio@users.noreply.github.com>
2008-05-16 17:07:04 +0000

    juliocesar: Quinto avance para el envio de "emails"

--- workflow/engine/menus/cases.php
+++ workflow/engine/menus/cases.php
@@ -25,12 +25,16 @@
  GLOBAL $RBAC;
   global $G_TMP_MENU;
 
+  if($RBAC->userCanAccess('PM_SUPERVISOR') == 1) {
+    $G_TMP_MENU->AddIdRawOption('CASES_TO_REVISE', 'cases/cases_List?l=to_revise', G::LoadTranslation('ID_TO_REVISE'));
+  }
   $G_TMP_MENU->AddIdRawOption('CASES_TO_DO', 'cases/cases_List?l=to_do', G::LoadTranslation('ID_TO_DO'));
   $G_TMP_MENU->AddIdRawOption('CASES_DRAFT', 'cases/cases_List?l=draft', G::LoadTranslation('ID_DRAFT'));
   $G_TMP_MENU->AddIdRawOption('CASES_PAUSED', 'cases/cases_List?l=paused', G::LoadTranslation('ID_PAUSED'));
   $G_TMP_MENU->AddIdRawOption('CASES_CANCELLED', 'cases/cases_List?l=cancelled', G::LoadTranslation('ID_CANCELLED'));
   $G_TMP_MENU->AddIdRawOption('CASES_COMPLETED', 'cases/cases_List?l=completed', G::LoadTranslation('ID_COMPLETED'));
   $G_TMP_MENU->AddIdRawOption('CASES_ALL', 'cases/cases_List?l=all', G::LoadTranslation('ID_ALL'));
-  if($RBAC->userCanAccess('PM_ALLCASES')==1)
-     $G_TMP_MENU->AddIdRawOption('CASES_GRAL', 'cases/cases_List?l=gral', G::LoadTranslation('ID_GENERAL'));
+  if($RBAC->userCanAccess('PM_ALLCASES') == 1) {
+    $G_TMP_MENU->AddIdRawOption('CASES_GRAL', 'cases/cases_List?l=gral', G::LoadTranslation('ID_GENERAL'));
+  }
 ?>
