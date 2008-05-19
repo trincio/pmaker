38785ab35199a54d724ee9eac470db6517e226b0
ferontiveros <trincio@users.noreply.github.com>
2008-05-19 22:38:31 +0000

    everth: supervisor

--- workflow/engine/classes/model/ProcessUser.php
+++ workflow/engine/classes/model/ProcessUser.php
@@ -15,5 +15,72 @@ require_once 'classes/model/om/BaseProcessUser.php';
  * @package    classes.model
  */
 class ProcessUser extends BaseProcessUser {
-
+	public function create($aData)
+  {
+  	$oConnection = Propel::getConnection(ProcessUserPeer::DATABASE_NAME);
+  	try {    	
+      $criteria = new Criteria('workflow');
+      $criteria->add(ProcessUserPeer::PU_UID,  $aData['PU_UID'] );
+      $criteria->add(ProcessUserPeer::PRO_UID,  $aData['PRO_UID']  );
+      $criteria->add(ProcessUserPeer::USR_UID,  $aData['USR_UID']  );
+      $criteria->add(ProcessUserPeer::PU_TYPE,  $aData['PU_TYPE']  );
+      $objects = ProcessUserPeer::doSelect($criteria, $oConnection);
+      $oConnection->begin();
+      foreach($objects as $row) {
+        $this->remove($row->getTasUid(), $row->getUsrUid(), $row->getTuType(), $row->getTuRelation() );
+      }
+      $oConnection->commit();
+          	
+  		
+  	  $oProcessUser = new ProcessUser();
+  	  $oProcessUser->fromArray($aData, BasePeer::TYPE_FIELDNAME);
+  	  if ($oProcessUser->validate()) {
+        $oConnection->begin();
+        $iResult = $oProcessUser->save();
+        $oConnection->commit();
+        return $iResult;
+  	  }
+  	  else {
+  	  	$sMessage = '';
+  	    $aValidationFailures = $oProcessUser->getValidationFailures();
+  	    foreach($aValidationFailures as $oValidationFailure) {
+          $sMessage .= $oValidationFailure->getMessage() . '<br />';
+        }
+        throw(new Exception('The registry cannot be created!<br />'.$sMessage));
+  	  }
+  	}
+    catch (Exception $oError) {
+      $oConnection->rollback();
+    	throw($oError);
+    }
+  }
+  
+  /**
+	 * Remove the application document registry
+   * @param string $sPuUid
+   * @param string $sProUid
+   * @param string $sUserUid
+   * @return string
+  **/
+  public function remove($sPuUid, $sProUid, $sUserUid, $PuType)
+  {
+  	$oConnection = Propel::getConnection(TaskUserPeer::DATABASE_NAME);
+  	try {
+  	  $oProcessUser = ProcessUserPeer::retrieveByPK($sPuUid, $sProUid, $sUserUid, $PuType);
+  	  if (!is_null($oProcessUser))
+  	  {
+  	  	$oConnection->begin();
+        $iResult = $oProcessUser->delete();
+        $oConnection->commit();
+        return $iResult;
+      }
+      else {
+        throw(new Exception('This row doesn\'t exists!'));
+      }
+    }
+    catch (Exception $oError) {
+    	$oConnection->rollback();
+      throw($oError);
+    }
+  }
 } // ProcessUser
