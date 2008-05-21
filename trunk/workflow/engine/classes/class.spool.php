d9b83bb913d4417b79ea3321148327178adb25c4
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-05-21 17:08:47 +0000

    juliocesar: MAILS, tercer avance

--- trunk/workflow/engine/classes/class.spool.php
+++ trunk/workflow/engine/classes/class.spool.php
@@ -24,7 +24,7 @@
 
 	class spoolRun
 	{
-
+    private $config;
 		private $fileData;
 		private $spool_id;
 		public  $status;
@@ -35,9 +35,10 @@
 		  $this->config   = array();
 			$this->fileData = array();
 			$this->spool_id = '';
+			$this->status   = 'pending';
+			$this->error    = '';
 
 			//$this->getSpoolFilesList();
-
 		}
 
 		public function getSpoolFilesList()
@@ -65,9 +66,15 @@
 			}
 		}
 
-		public function setConfig($sServer, $sPort) {
-		  $this->config['server'] = $sServer;
-		  $this->config['port']   = $sPort;
+		public function create($aData) {
+		  G::LoadClass('insert');
+      $oInsert = new insert();
+      $sUID    = $oInsert->db_insert($aData);
+      $this->setData($sUID, $aData['app_msg_subject'], $aData['app_msg_from'], $aData['app_msg_to'], $aData['app_msg_body']);
+		}
+
+		public function setConfig($aConfig) {
+		  $this->config = $aConfig;
 		}
 
 		public function setData($sAppMsgUid, $sSubject, $sFrom, $sTo, $sBody, $sDate = '', $sCC = '', $sBCC = '', $sTemplate = '') {
@@ -80,7 +87,7 @@
 			$this->fileData['cc'] 		     = $sCC;
 			$this->fileData['bcc'] 		     = $sBCC;
 			$this->fileData['template'] 	 = $sTemplate;
-			$this->fileData['attachments'] = array(); //$rs->getString('APP_MSG_ATTACH');
+			$this->fileData['attachments'] = array();
 		  $this->fileData['domain'] 	   = gethostbyaddr('127.0.0.1');
 		}
 
@@ -93,13 +100,9 @@
 
 		private function updateSpoolStatus()
 		{
-			(false === $this->status)
-				? $s = 'failed'
-				: $s = 'sent';
-
-			$spool = AppMessagePeer::retrieveByPK($this->spool_id);
-			$spool->setappMsgstatus($s);
-			$spool->save();
+			$oAppMessage = AppMessagePeer::retrieveByPK($this->spool_id);
+			$oAppMessage->setappMsgstatus($this->status);
+			$oAppMessage->save();
 
 		}
 
@@ -109,7 +112,7 @@
 			{
 				$this->fileData['from_name']  = trim(substr($this->fileData['from'],0,$pos));
 				$this->fileData['from_email'] = trim(substr($this->fileData['from'],$pos));
-
+        $this->fileData['from_email'] = str_replace('<', '', str_replace('>', '', $this->fileData['from_email']));
 			}
 
 		}
@@ -143,31 +146,72 @@
 		{
 			if(count($this->fileData['envelope_to'])>0)
 			{
-			  //obtener la configuración del envio de mails, y segun eso enviar para cada tipo
-        //pero por el momento solo para OPENMAIL
-        G::LoadClass('package');
-        G::LoadClass('smtp');
-				$pack = new package($this->fileData);
-				$header = $pack->returnHeader();
-				$body   = $pack->returnBody();
-				unset($pack);
-				$return_path = '<'."{$this->fileData['from_email']}".'>';
-				$send = new smtp();
-				$send->setServer($this->config['server']);
-				$send->setPort($this->config['port']);
-				$send->setReturnPath($return_path);
-				$send->setHeaders($header);
-				$send->setBody($body);//var_dump($this->fileData['envelope_to']);die;
-				$send->getEnvelopeTo($this->fileData['envelope_to']);
-				$this->status = $send->returnStatus();
-				if (!$send->sendMessage()) {
-				  $this->error = implode(', ', $send->returnErrors());
-				}
-				else {
-				  $this->error = '';
-				}
-				/*$send = new smtp($return_path, $this->fileData['envelope_to'], $header, $body);*/
-				unset($send);
+        switch ($this->config['MESS_ENGINE']) {
+          case 'MAIL':
+            G::LoadThirdParty('phpmailer', 'class.phpmailer');
+            $oPHPMailer = new PHPMailer();
+            $oPHPMailer->Mailer   = 'mail';
+            $oPHPMailer->From     = $this->fileData['from_email'];
+            $oPHPMailer->FromName = $this->fileData['from_name'];
+            $oPHPMailer->Subject  = $this->fileData['subject'];
+            $oPHPMailer->Body     = $this->fileData['body'];
+            $oPHPMailer->AddAddress($this->fileData['to']);
+            if ($oPHPMailer->Send()) {
+              $this->error = '';
+				      $this->status = 'sent';
+            }
+            else {
+              $this->error = $oPHPMailer->ErrorInfo;
+				      $this->status = 'failed';
+            }
+          break;
+          case 'PHPMAILER':
+            G::LoadThirdParty('phpmailer', 'class.phpmailer');
+            $oPHPMailer = new PHPMailer();
+            $oPHPMailer->Mailer   = 'smtp';
+            $oPHPMailer->SMTPAuth = true;
+            $oPHPMailer->Host     = $this->config['MESS_SERVER'];
+            $oPHPMailer->Port     = $this->config['MESS_PORT'];
+            $oPHPMailer->Username = $this->config['MESS_ACCOUNT'];
+            $oPHPMailer->Password = $this->config['MESS_PASSWORD'];
+            $oPHPMailer->From     = $this->fileData['from_email'];
+            $oPHPMailer->FromName = $this->fileData['from_name'];
+            $oPHPMailer->Subject  = $this->fileData['subject'];
+            $oPHPMailer->Body     = $this->fileData['body'];
+            $oPHPMailer->AddAddress($this->fileData['to']);
+            $oPHPMailer->IsHTML(true);
+            if ($oPHPMailer->Send()) {
+              $this->error = '';
+				      $this->status = 'sent';
+            }
+            else {
+              $this->error = $oPHPMailer->ErrorInfo;
+				      $this->status = 'failed';
+            }
+          break;
+          case 'OPENMAIL':
+            G::LoadClass('package');
+            G::LoadClass('smtp');
+				    $pack = new package($this->fileData);
+				    $header = $pack->returnHeader();
+				    $body   = $pack->returnBody();
+				    $send = new smtp();
+				    $send->setServer($this->config['MESS_SERVER']);
+				    $send->setPort($this->config['MESS_PORT']);
+				    $send->setReturnPath($this->fileData['from_email']);
+				    $send->setHeaders($header);
+				    $send->setBody($body);
+				    $send->getEnvelopeTo($this->fileData['envelope_to']);
+				    if ($send->sendMessage()) {
+				      $this->error = '';
+				      $this->status = 'sent';
+				    }
+				    else {
+				      $this->error = implode(', ', $send->returnErrors());
+				      $this->status = 'failed';
+				    }
+          break;
+        }
 			}
 		}
 	} // end of class
