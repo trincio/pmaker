6df5f3f696f1cc0671e525289ffeb8dd09f7c981
ferontiveros <trincio@users.noreply.github.com>
2008-05-24 16:49:14 +0000

    juliocesar: M?\195?\161s arreglos

--- workflow/engine/classes/class.report.php
+++ workflow/engine/classes/class.report.php
@@ -33,161 +33,173 @@
 
 class Report {
 	function generatedReport1()
-	{ 
-		require_once 'classes/model/AppDelegation.php';	
-		require_once 'classes/model/Application.php';		
+	{
+		require_once 'classes/model/AppDelegation.php';
+		require_once 'classes/model/Application.php';
 		$oCriteria = new Criteria('workflow');
 		$del = DBAdapter::getStringDelimiter();
-    $oCriteria->addSelectColumn(AppDelegationPeer::PRO_UID);        
+    $oCriteria->addSelectColumn(AppDelegationPeer::PRO_UID);
     $oCriteria->addAsColumn("TOTALDUR", "SUM(".AppDelegationPeer::DEL_DURATION.")");
-    $oCriteria->addAsColumn("PROMEDIO", "AVG(".AppDelegationPeer::DEL_DURATION.")");          
-    $oCriteria->addAsColumn('PRO_TITLE', 'C1.CON_VALUE' );            
-    $oCriteria->addAlias("C1",  'CONTENT');            
+    $oCriteria->addAsColumn("PROMEDIO", "AVG(".AppDelegationPeer::DEL_DURATION.")");
+    $oCriteria->addAsColumn('PRO_TITLE', 'C1.CON_VALUE' );
+    $oCriteria->addAlias("C1",  'CONTENT');
     $proTitleConds = array();
     $proTitleConds[] = array( AppDelegationPeer::PRO_UID , 'C1.CON_ID' );
     $proTitleConds[] = array( 'C1.CON_CATEGORY' , $del . 'PRO_TITLE' . $del );
     $proTitleConds[] = array( 'C1.CON_LANG' ,     $del . SYS_LANG . $del );
-    $oCriteria->addJoinMC($proTitleConds ,    Criteria::LEFT_JOIN);		              
-    $oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID); 
-    
+    $oCriteria->addJoinMC($proTitleConds ,    Criteria::LEFT_JOIN);
+    $oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID);
+
     $oDataset = AppDelegationPeer::doSelectRS($oCriteria);
     $oDataset->setFetchmode(ResultSet::FETCHMODE_ASSOC);
     $oDataset->next();
+    $aProcess[] = array('PRO_UID'   => 'char',
+      	                'PRO_TITLE' => 'char',
+      	                'CANTCASES' => 'integer',
+      	                'TOTALDUR'  => 'float',
+      	                'PROMEDIO'  => 'float');
     while ($aRow = $oDataset->getRow()) {
-      	$oCriteria = new Criteria('workflow');		
-        $oCriteria->addSelectColumn(ApplicationPeer::PRO_UID);        
-        //$oCriteria->addAsColumn("CANTCASES", "COUNT(".ApplicationPeer::PRO_UID.")");                       
+      	$oCriteria = new Criteria('workflow');
+        $oCriteria->addSelectColumn(ApplicationPeer::PRO_UID);
+        //$oCriteria->addAsColumn("CANTCASES", "COUNT(".ApplicationPeer::PRO_UID.")");
         $oCriteria->add(ApplicationPeer::PRO_UID,     $aRow['PRO_UID']);
-        //$oCriteria->addGroupByColumn(ApplicationPeer::PRO_UID);       	
-        
+        //$oCriteria->addGroupByColumn(ApplicationPeer::PRO_UID);
+
       	$aProcess[] = array('PRO_UID'   => $aRow['PRO_UID'],
       	                    'PRO_TITLE' => $aRow['PRO_TITLE'],
       	                    'CANTCASES' => ApplicationPeer::doCount($oCriteria),
       	                    'TOTALDUR'  => $aRow['TOTALDUR'],
-      	                    'PROMEDIO'  => $aRow['PROMEDIO']);      	                  
+      	                    'PROMEDIO'  => $aRow['PROMEDIO']);
       	$oDataset->next();
      }
-    
+
     global $_DBArray;
     $_DBArray['reports']  = $aProcess;
     $_SESSION['_DBArray'] = $_DBArray;
     G::LoadClass('ArrayPeer');
     $oCriteria = new Criteria('dbarray');
-    $oCriteria->setDBArrayTable('reports');          	    
-        
+    $oCriteria->setDBArrayTable('reports');
+
     return $oCriteria;
-	}	
-	
+	}
+
 	function descriptionReport1($PRO_UID)
 	{
 		require_once 'classes/model/AppDelegation.php';
 		require_once 'classes/model/Task.php';
 		require_once 'classes/model/Content.php';
-		
+
 		$oCriteria = new Criteria('workflow');
 		$del = DBAdapter::getStringDelimiter();
 		$oCriteria->addSelectColumn(AppDelegationPeer::PRO_UID);
 		$oCriteria->addAsColumn("TOTALDUR", "SUM(".AppDelegationPeer::DEL_DURATION.")");
-		$oCriteria->addAsColumn("PROMEDIO", "AVG(".AppDelegationPeer::DEL_DURATION.")");   
-		          
+		$oCriteria->addAsColumn("PROMEDIO", "AVG(".AppDelegationPeer::DEL_DURATION.")");
+
 		$oCriteria->addJoin(AppDelegationPeer::TAS_UID, TaskPeer::TAS_UID, Criteria::LEFT_JOIN);
-		 
-		$oCriteria->addAsColumn('TAS_TITLE', 'C.CON_VALUE');       
-		$oCriteria->addAlias("C", 'CONTENT');  
-		 
+
+		$oCriteria->addAsColumn('TAS_TITLE', 'C.CON_VALUE');
+		$oCriteria->addAlias("C", 'CONTENT');
+
 		$proContentConds = array();
 		$proContentConds[] = array(AppDelegationPeer::TAS_UID, 'C.CON_ID');
 		$proContentConds[] = array('C.CON_CATEGORY', $del . 'TAS_TITLE' . $del);
 		$proContentConds[] = array('C.CON_LANG',     $del . SYS_LANG . $del);
 		$oCriteria->addJoinMC($proContentConds,      Criteria::LEFT_JOIN);
-		
+
 		$oCriteria->add(AppDelegationPeer::PRO_UID, $PRO_UID);
-		
-		$oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID); 
-		$oCriteria->addGroupByColumn('C.CON_VALUE'); 	
-		
+
+		$oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID);
+		$oCriteria->addGroupByColumn('C.CON_VALUE');
+
 		return $oCriteria;
 	}
-	
+
 	function generatedReport2()
-	{ 
-		require_once 'classes/model/AppDelegation.php';	
-		require_once 'classes/model/Application.php';		
+	{
+		require_once 'classes/model/AppDelegation.php';
+		require_once 'classes/model/Application.php';
 		$oCriteria = new Criteria('workflow');
 		$del = DBAdapter::getStringDelimiter();
-    $oCriteria->addSelectColumn(AppDelegationPeer::PRO_UID);                    
-    $oCriteria->addAsColumn('PRO_TITLE', 'C1.CON_VALUE' );            
-    $oCriteria->addAlias("C1",  'CONTENT');            
+    $oCriteria->addSelectColumn(AppDelegationPeer::PRO_UID);
+    $oCriteria->addAsColumn('PRO_TITLE', 'C1.CON_VALUE' );
+    $oCriteria->addAlias("C1",  'CONTENT');
     $proTitleConds = array();
     $proTitleConds[] = array( AppDelegationPeer::PRO_UID , 'C1.CON_ID' );
     $proTitleConds[] = array( 'C1.CON_CATEGORY' , $del . 'PRO_TITLE' . $del );
     $proTitleConds[] = array( 'C1.CON_LANG' ,     $del . SYS_LANG . $del );
-    $oCriteria->addJoinMC($proTitleConds ,    Criteria::LEFT_JOIN);		              
-    $oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID);                   	    
+    $oCriteria->addJoinMC($proTitleConds ,    Criteria::LEFT_JOIN);
+    $oCriteria->addGroupByColumn(AppDelegationPeer::PRO_UID);
 
     $oDataset = AppDelegationPeer::doSelectRS($oCriteria);
     $oDataset->setFetchmode(ResultSet::FETCHMODE_ASSOC);
     $oDataset->next();
-    
+
     $lastmonth = mktime(0, 0, 0, date("m")-1  , date("d"), date("Y"));
     $month = date( 'Y-m-d' , $lastmonth );
-    
+
     $lastday = mktime(0, 0, 0, date("m")  , date("d")-1, date("Y"));
     $day = date( 'Y-m-d' , $lastday );
-    
-    while ($aRow = $oDataset->getRow()) {      	                    	
-      	$oCriteria2 = new Criteria('workflow');		
-      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);  
-        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");               
-        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);  
-        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);        	        
+
+    $aProcess[] = array('PRO_UID'   => 'char',
+      	                'PRO_TITLE' => 'char',
+      	                'CANTCASES' => 'integer',
+      	                'CASELASTMONTH' => 'integer',
+      	                'CASELASTDAY' => 'integer'
+      	                );
+
+    while ($aRow = $oDataset->getRow()) {
+      	$oCriteria2 = new Criteria('workflow');
+      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);
+        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");
+        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);
+        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);
         $oDataset2 = AppDelegationPeer::doSelectRS($oCriteria2);
         $oDataset2->setFetchmode(ResultSet::FETCHMODE_ASSOC);
         $oDataset2->next();
-        $aRow2 = $oDataset2->getRow();     
+        $aRow2 = $oDataset2->getRow();
         $cant = $aRow2['CANTCASES'];
-        
-      	$oCriteria2 = new Criteria('workflow');		
-      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);  
-        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");               
-        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);          	        
-        $oCriteria2->add(ApplicationPeer::APP_INIT_DATE, $month, Criteria::GREATER_EQUAL); 
-        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);  
+
+      	$oCriteria2 = new Criteria('workflow');
+      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);
+        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");
+        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);
+        $oCriteria2->add(ApplicationPeer::APP_INIT_DATE, $month, Criteria::GREATER_EQUAL);
+        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);
         $oDataset2 = AppDelegationPeer::doSelectRS($oCriteria2);
         $oDataset2->setFetchmode(ResultSet::FETCHMODE_ASSOC);
         $oDataset2->next();
-        $aRow2 = $oDataset2->getRow();     
+        $aRow2 = $oDataset2->getRow();
         $cant1 = $aRow2['CANTCASES'];
-        
-        $oCriteria2 = new Criteria('workflow');		
-      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);  
-        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");               
-        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);          	        
-        $oCriteria2->add(ApplicationPeer::APP_INIT_DATE, $day, Criteria::GREATER_EQUAL); 
-        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);  
+
+        $oCriteria2 = new Criteria('workflow');
+      	$oCriteria2->addSelectColumn(ApplicationPeer::PRO_UID);
+        $oCriteria2->addAsColumn("CANTCASES", "COUNT(*)");
+        $oCriteria2->add(ApplicationPeer::PRO_UID, $aRow['PRO_UID']);
+        $oCriteria2->add(ApplicationPeer::APP_INIT_DATE, $day, Criteria::GREATER_EQUAL);
+        $oCriteria2->addGroupByColumn(ApplicationPeer::PRO_UID);
         $oDataset2 = AppDelegationPeer::doSelectRS($oCriteria2);
         $oDataset2->setFetchmode(ResultSet::FETCHMODE_ASSOC);
         $oDataset2->next();
-        $aRow2 = $oDataset2->getRow();     
+        $aRow2 = $oDataset2->getRow();
         $cant2 = $aRow2['CANTCASES'];
-              	      	
+
       	$aProcess[] = array('PRO_UID'   => $aRow['PRO_UID'],
       	                    'PRO_TITLE' => $aRow['PRO_TITLE'],
       	                    'CANTCASES' => $cant,
       	                    'CASELASTMONTH' => $cant1,
       	                    'CASELASTDAY' => $cant2
-      	                   );      	                  
+      	                   );
       	$oDataset->next();
      }
-    
+
     global $_DBArray;
     $_DBArray['reports']  = $aProcess;
     $_SESSION['_DBArray'] = $_DBArray;
     G::LoadClass('ArrayPeer');
     $oCriteria = new Criteria('dbarray');
-    $oCriteria->setDBArrayTable('reports');             
+    $oCriteria->setDBArrayTable('reports');
     return $oCriteria;
-	}	
+	}
 }
 
 
