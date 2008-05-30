0b1adb578df40b77ff4dbecc6a64659ab698f112
ferontiveros <trincio@users.noreply.github.com>
2008-05-30 17:16:28 +0000

    juliocesar: Se amument?\195?\179 la exportaci?\195?\179n de conexiones a bases de datos

--- workflow/engine/methods/processes/processes_Export.php
+++ workflow/engine/methods/processes/processes_Export.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * processes_Export.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
 G::LoadThirdParty('pear/json','class.json');
@@ -29,11 +29,11 @@ try {
 
   $oJSON = new Services_JSON();
   $stdObj = $oJSON->decode( $_POST['data'] );
-  if ( isset ($stdObj->pro_uid ) ) 
+  if ( isset ($stdObj->pro_uid ) )
     $sProUid = $stdObj->pro_uid;
   else
     throw ( new Exception ( 'the process uid is not defined!.' ) );
- 
+
 /* Includes */
 G::LoadClass('processes');
 $oProcess  = new Processes();
