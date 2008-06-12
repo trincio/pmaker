d4b392b795bce4e94454750edef698b09154ede2
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-06-12 22:50:36 +0000

    juliocesar: Se mejoro la parte de las leyendas cuando insertas variables de los formularios

--- trunk/workflow/engine/classes/model/Task.php
+++ trunk/workflow/engine/classes/model/Task.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Task.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 
 require_once 'classes/model/om/BaseTask.php';
@@ -267,7 +267,7 @@ class Task extends BaseTask {
     }
     return 0;
   }
-  
+
   /**
    * create a new Task
    *
@@ -290,7 +290,7 @@ class Task extends BaseTask {
       $this->setTasTypeDay("");
       $this->setTasTimeunit("DAYS");
       $this->setTasAlert("FALSE");
-      $this->setTasPriorityVariable("");
+      $this->setTasPriorityVariable("@@SYS_CASE_PRIORITY");
       $this->setTasAssignType("BALANCED");
       $this->setTasAssignVariable("@@SYS_NEXT_USER_TO_BE_ASSIGNED");
       $this->setTasAssignLocation("FALSE");
@@ -342,8 +342,8 @@ class Task extends BaseTask {
       throw($e);
     }
   }
-  
-  
+
+
   public function load($TasUid)
   {
     try {
@@ -452,5 +452,5 @@ class Task extends BaseTask {
     	throw($oError);
     }
   }
- 
+
 } // Task
