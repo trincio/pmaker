0543eb4e0107d6a2d6b72dd9b2b1012bf5fb25f0
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-06-23 23:20:04 +0000

    juliocesar: Cuarto avanze para la configuraci?\195?\179n de "dashboards", ya es funcional, solo falta la configuraci?\195?\179n

--- trunk/workflow/engine/js/dashboard/core/dashboard.js
+++ trunk/workflow/engine/js/dashboard/core/dashboard.js
@@ -1,4 +1,5 @@
 var oPanel;
+var oDashboards = {};
 var newDashboard = function() {
   oPanel = new leimnud.module.panel();
   oPanel.options = {
@@ -37,6 +38,24 @@ var addDashboard = function(aDashboard) {
   oRPC.callback = function(rpc){
   	oPanel.loader.hide();
   	oPanel.remove();
+    eval(rpc.xmlhttp.responseText);
+    $("dashboard").innerHTML = '';
+  	window.Da=new leimnud.module.dashboard();
+  	Da.make({target:$("dashboard"),data:oDashboards});
+  }.extend(this);
+  oRPC.make();
+};
+
+var removeDashboard = function(sClass, sChart) {
+  var oRPC = new leimnud.module.rpc.xmlhttp({
+  	url : 'dashboardAjax',
+  	args: 'action=removeDashboard&sDashboardClass=' + sClass + '&sChart=' + sChart
+  });
+  oRPC.callback = function(rpc){
+    eval(rpc.xmlhttp.responseText);
+    $("dashboard").innerHTML = '';
+  	window.Da=new leimnud.module.dashboard();
+  	Da.make({target:$("dashboard"),data:oDashboards});
   }.extend(this);
   oRPC.make();
 };
