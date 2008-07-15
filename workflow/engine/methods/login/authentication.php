42fac7f1e6a5a75c569e9f59bc31a04c6662bcb9
ferontiveros <trincio@users.noreply.github.com>
2008-07-15 20:57:26 +0000

    juliocesar: Se corrigi?\195?\179 un problema que habia cuando un rol tenia solo determinados permisos

--- workflow/engine/methods/login/authentication.php
+++ workflow/engine/methods/login/authentication.php
@@ -31,19 +31,14 @@
 
 
 try {
-	$frm  = $_POST['form'];
-	if ( isset ( $frm['USR_USERNAME'] ) ) 	{
-	  $usr = strtolower( trim( $frm['USR_USERNAME']));
-	  $pwd = trim( $frm['USR_PASSWORD']);
+	$frm = $_POST['form'];
+	$usr = '';
+	$pwd = '';
+	if (isset($frm['USR_USERNAME'])) {
+	  $usr = strtolower(trim($frm['USR_USERNAME']));
+	  $pwd = trim($frm['USR_PASSWORD']);
 	}
-  else
-	{
-		/*$usr = $_SESSION['USER_TEMP'];
-	  $pwd = $_SESSION['PASS_TEMP'];
-	  unset( $_SESSION['PASS_TEMP']);
-		unset( $_SESSION['USER_TEMP']);*/
-	}
-	$uid  = $RBAC->VerifyLogin( $usr , $pwd);
+	$uid = $RBAC->VerifyLogin($usr , $pwd);
 	switch ($uid) {
 		//The user not exists
 	  case -1:
@@ -67,13 +62,12 @@ try {
 	  die;
 	}
 
-	$_SESSION['USER_LOGGED'] = $uid;
+	$_SESSION['USER_LOGGED']  = $uid;
 	$_SESSION['USR_USERNAME'] = $usr;
 
   // Asign the uid of user to userloggedobj
-  $RBAC->loadUserRolePermission( $RBAC->sSystem, $uid );
-	$res = $RBAC->userCanAccess("PM_LOGIN");
-
+  $RBAC->loadUserRolePermission($RBAC->sSystem, $uid);
+	$res = $RBAC->userCanAccess('PM_LOGIN');
 
 	if ($res != 1 ) {
 	  if ($res == -2)
@@ -98,49 +92,37 @@ try {
   	}
   }
 
-  //TODO:
-  /****************** THIS LINE IS NEEDED UNTIL THERE WERE FACOTORY ,...*/
-  /*
-  $_SESSION['USER_ROLE']  = 'ADMIN';
-  $frm['USER_LANG'] = (isset($frm['USER_LANG']) && $frm['USER_LANG'])?$frm['USER_LANG']:
-    defined('SYS_LANG')?SYS_LANG:'en';
-  $_SESSION['USR_USERNAME'] = $frm['USR_USERNAME'];
-  G::header('location: /sys' .  SYS_TEMP . '/' . $frm['USER_LANG'] . '/' . SYS_SKIN . '/' . 'cases/cases_List'); die;
-  */
-  /****************** THIS LINE IS NEEDED UNTIL THERE WERE FACOTORY ,...*/
-
-	//G::LoadClass('log');
-	//$log = new Log;
-	//$log->SaveLogin ( $uid, $usr );
-
-	//if ( file_exists ( PATH_METHODS . 'login/'. SYS_SYS . ".php" ) ) {
-	//  include ( SYS_SYS . ".php" );
-	//}
-
-	$accessPMProcess   = $RBAC->userCanAccess("PM_FACTORY");
-	$accessPMCases     = $RBAC->userCanAccess("PM_CASES");
-
-	//if ( $accessWfArchitect == 1 )
-	//  $_SESSION['USER_ROLE']  = 'ARCHITECT';
-	//else if ( $accessWfProcess == 1 )
-	//    $_SESSION['USER_ROLE']  = 'ADMIN';
-	//  else
-	//    $_SESSION['USER_ROLE']  = 'USER';
-
-	//administrator
-	if ( $accessPMProcess == 1) {
+	$res = $RBAC->userCanAccess('PM_FACTORY');
+	if ($res == 1) {
     G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'processes/processes_List');
 	  die;
 	}
 
-	//Operador
-	if ( $accessPMCases == 1) {
+	$res = $RBAC->userCanAccess('PM_CASES');
+	if ($res == 1) {
     G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'cases/cases_List');
 	  die;
 	}
 
+	$res = $RBAC->userCanAccess('PM_REPORTS');
+	if ($res == 1) {
+    G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'reports/reportsList');
+	  die;
+	}
 
-	throw ( new Exception ( "the user $usr has no role assigned  ($res, $uid)" ) );
+	$res = $RBAC->userCanAccess('PM_USERS');
+	if ($res == 1) {
+    G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'users/users_List');
+	  die;
+	}
+
+	$res = $RBAC->userCanAccess('PM_SETUP');
+	if ($res == 1) {
+    G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'setup/pluginsList');
+	  die;
+	}
+
+	G::header('location: /sys' .  SYS_TEMP . '/' . $lang . '/' . SYS_SKIN . '/' . 'users/myInfo');
 
 }
 catch ( Exception $e ) {
