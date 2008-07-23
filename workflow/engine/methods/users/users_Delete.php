4952573d5194ccbf099493612680b875c03bfc80
ferontiveros <trincio@users.noreply.github.com>
2008-07-23 20:09:18 +0000

    juliocesar: Se arregl?\195?\179 el bug 0002894

--- workflow/engine/methods/users/users_Delete.php
+++ workflow/engine/methods/users/users_Delete.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * users_Delete.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 try {
   global $RBAC;
@@ -51,12 +51,12 @@ try {
   $oTasks->ofToAssignUserOfAllTasks($_GET['USR_UID']);
   G::LoadClass('groups');
   $oGroups = new Groups();
-  $oGroups->removeUserOfAllGroups($_GET['USR_UID']);  
+  $oGroups->removeUserOfAllGroups($_GET['USR_UID']);
   $RBAC->changeUserStatus($_GET['USR_UID'], 'CLOSED');
   $_GET['USR_USERNAME']='';
   $RBAC->updateUser(array('USR_UID' => $_GET['USR_UID'], 'USR_USERNAME' => $_GET['USR_USERNAME']),'');
-  
-  
+
+
   require_once 'classes/model/Users.php';
   $oUser                 = new Users();
   $aFields               = $oUser->load($_GET['USR_UID']);
