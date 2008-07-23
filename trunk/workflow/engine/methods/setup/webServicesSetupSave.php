58cb4bc3c5e442276b70150081ca8e57c3ef994a
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2008-07-23 15:05:51 +0000

    everth: ws

--- trunk/workflow/engine/methods/setup/webServicesSetupSave.php
+++ trunk/workflow/engine/methods/setup/webServicesSetupSave.php
@@ -1,5 +1,5 @@
 <?
-
+unset($_SESSION['WS_SESSION_ID']);
 $_SESSION['END_POINT']=$_POST['form']['WS_PROTOCOL'].'://' .$_POST['form']['WS_HOST'] . ':' .$_POST['form']['WS_PORT'] .'/sys' .$_POST['form']['WS_WORKSPACE'].'/en/green/services/wsdl';
 G::header('location: webServices');
 
