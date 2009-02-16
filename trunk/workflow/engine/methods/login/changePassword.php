3998b29b8d554dd9390b9168c66d515158d04966
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2009-02-16 16:35:47 +0000

    juliocesar: Se aument?\195?\179 las pol?\195?\173ticas para paswords de usuarios (segunda parte)

---
+++ trunk/workflow/engine/methods/login/changePassword.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'classes/model/Users.php';
+$oUser = new Users();
+$aUser = $oUser->load($_SESSION['USER_LOGGED']);
+global $RBAC;
+$aData['USR_UID']      = $aUser['USR_UID'];
+$aData['USR_USERNAME'] = $aUser['USR_USERNAME'];
+$aData['USR_PASSWORD'] = md5($_POST['form']['USR_PASSWORD']);
+$aData['USR_FIRSTNAME']   = $aUser['USR_FIRSTNAME'];
+$aData['USR_LASTNAME']    = $aUser['USR_LASTNAME'];
+$aData['USR_EMAIL']       = $aUser['USR_EMAIL'];
+$aData['USR_DUE_DATE']    = $aUser['USR_DUE_DATE'];
+$aData['USR_UPDATE_DATE'] = date('Y-m-d H:i:s');
+$RBAC->updateUser($aData, $aUser['USR_ROLE']);
+$aData['USR_COUNTRY']     = $aUser['USR_COUNTRY'];
+$aData['USR_CITY']        = $aUser['USR_CITY'];
+$aData['USR_LOCATION']    = $aUser['USR_LOCATION'];
+$aData['USR_ADDRESS']     = $aUser['USR_ADDRESS'];
+$aData['USR_PHONE']       = $aUser['USR_PHONE'];
+$aData['USR_ZIP_CODE']    = $aUser['USR_ZIP_CODE'];
+$aData['USR_POSITION']    = $aUser['USR_POSITION'];
+$oUser->update($aData);
+if ( class_exists('redirectDetail')) {
+  //falta validar...
+  if(isset($RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE']))
+  		$userRole = $RBAC->aUserInfo['PROCESSMAKER']['ROLE']['ROL_CODE'];
+
+  $oPluginRegistry = &PMPluginRegistry::getSingleton();
+  //$oPluginRegistry->showArrays();
+  $aRedirectLogin = $oPluginRegistry->getRedirectLogins();
+  if(isset($aRedirectLogin))
+	 { if(is_array($aRedirectLogin))
+	 	 {
+	 	 		foreach ( $aRedirectLogin as $key=>$detail ) {
+		  	   if(isset($detail->sPathMethod))
+			  	  {
+			  	  	if ( $detail->sRoleCode == $userRole ) {
+			       	  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . $detail->sPathMethod );
+			       	  die;
+			  	   	}
+			  	  }
+	      }
+	   }
+   }
+}
+//end plugin
+
+$res = $RBAC->userCanAccess('PM_FACTORY');
+if ($res == 1) {
+  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'processes/processes_List');
+  die;
+}
+
+$res = $RBAC->userCanAccess('PM_CASES');
+if ($res == 1) {
+  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'cases/cases_List');
+  die;
+}
+
+$res = $RBAC->userCanAccess('PM_REPORTS');
+if ($res == 1) {
+  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'reports/reportsList');
+  die;
+}
+
+$res = $RBAC->userCanAccess('PM_USERS');
+if ($res == 1) {
+  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'users/users_List');
+  die;
+}
+
+$res = $RBAC->userCanAccess('PM_SETUP');
+if ($res == 1) {
+  G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'setup/pluginsList');
+  die;
+}
+
+G::header('location: /sys' .  SYS_TEMP . '/' . SYS_LANG . '/' . SYS_SKIN . '/' . 'users/myInfo');
