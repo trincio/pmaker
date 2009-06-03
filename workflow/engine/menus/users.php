cb28e0ce9d89a8ef1260642ab43e503621bfa4dd
ferontiveros <trincio@users.noreply.github.com>
2009-06-03 18:59:04 +0000

    juliocesar: La opci?\195?\179n de "Authentication Sources" necesita del permiso "PM_SETUP_ADVANCE", en la version "1.1-1.1.2153-1.2.2304", existia un peque?\195?\177o bug con el sub-menu del tab "USERS" el cual segu?\195?\173a mostrando la opci?\195?\179n nueva este o no este asignado el permiso

--- workflow/engine/menus/users.php
+++ workflow/engine/menus/users.php
@@ -29,7 +29,10 @@ $G_TMP_MENU->AddIdRawOption('USERS', 'users/users_List');
 //$G_TMP_MENU->AddIdRawOption('DEPARTMENTS', 'departments/departments_List');
 $G_TMP_MENU->AddIdRawOption('GROUPS', 'groups/groups');
 $G_TMP_MENU->AddIdRawOption('ROLES', 'roles/roles_List');
-$G_TMP_MENU->AddIdRawOption('AUTH_SOURCES', 'authSources/authSources_List');
+global $RBAC;
+if ($RBAC->userCanAccess('PM_SETUP_ADVANCE') == 1) {
+  $G_TMP_MENU->AddIdRawOption('AUTH_SOURCES', 'authSources/authSources_List');
+}
 
 $G_TMP_MENU->Labels = array(
   G::LoadTranslation('ID_USERS_LIST'),
