20a3b600a5933d4381a5ac301bac6d4499d8fec8
ferontiveros <ferontiveros@a6cc957b-8b46-0410-bee0-dbb4ac81c236>
2009-07-08 12:57:24 +0000

    juliocesar: Bug con el envio de correos a varias cuentas resuelto

--- trunk/workflow/engine/classes/class.spool.php
+++ trunk/workflow/engine/classes/class.spool.php
@@ -186,14 +186,21 @@
             $oPHPMailer->FromName = $this->fileData['from_name'];
             $oPHPMailer->Subject  = $this->fileData['subject'];
             $oPHPMailer->Body     = $this->fileData['body'];
-            if (strpos($this->fileData['to'], '<') !== false) {
-              $aTo     = explode('<', $this->fileData['to']);
-              $sToName = trim($aTo[0]);
-              $sTo     = trim(str_replace('>', '', $aTo[1]));
-              $oPHPMailer->AddAddress($sTo, $sToName);
+            if(count($this->fileData['envelope_to']) == 1) {
+              if (strpos($this->fileData['to'], '<') !== false) {
+                $aTo     = explode('<', $this->fileData['to']);
+                $sToName = trim($aTo[0]);
+                $sTo     = trim(str_replace('>', '', $aTo[1]));
+                $oPHPMailer->AddAddress($sTo, $sToName);
+              }
+              else {
+                $oPHPMailer->AddAddress($this->fileData['to']);
+              }
             }
             else {
-              $oPHPMailer->AddAddress($this->fileData['to']);
+              foreach ($this->fileData['envelope_to'] as $sEmail) {
+                $oPHPMailer->AddAddress($sEmail);
+              }
             }
             $oPHPMailer->IsHTML(true);
             if ($oPHPMailer->Send()) {
@@ -218,14 +225,21 @@
             $oPHPMailer->FromName = utf8_decode($this->fileData['from_name']);
             $oPHPMailer->Subject  = utf8_decode($this->fileData['subject']);
             $oPHPMailer->Body     = utf8_decode($this->fileData['body']);
-            if (strpos($this->fileData['to'], '<') !== false) {
-              $aTo     = explode('<', $this->fileData['to']);
-              $sToName = trim($aTo[0]);
-              $sTo     = trim(str_replace('>', '', $aTo[1]));
-              $oPHPMailer->AddAddress($sTo, utf8_decode($sToName));
+            if(count($this->fileData['envelope_to']) == 1) {
+              if (strpos($this->fileData['to'], '<') !== false) {
+                $aTo     = explode('<', $this->fileData['to']);
+                $sToName = trim($aTo[0]);
+                $sTo     = trim(str_replace('>', '', $aTo[1]));
+                $oPHPMailer->AddAddress($sTo, utf8_decode($sToName));
+              }
+              else {
+                $oPHPMailer->AddAddress($this->fileData['to']);
+              }
             }
             else {
-              $oPHPMailer->AddAddress($this->fileData['to']);
+              foreach ($this->fileData['envelope_to'] as $sEmail) {
+                $oPHPMailer->AddAddress($sEmail);
+              }
             }
             $oPHPMailer->IsHTML(true);
             if ($oPHPMailer->Send()) {
