0e8cc6201b3df41e712a9590ececceb4ac2e6396
ferontiveros <trincio@users.noreply.github.com>
2009-09-28 14:24:00 +0000

    juliocesar: Bug 3541 resuelto, se valid?\195?\179 para todos los tipos de objetos que tenemos, no solo para dynaforms

--- workflow/engine/methods/dynaforms/dynaforms_Delete.php
+++ workflow/engine/methods/dynaforms/dynaforms_Delete.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * dynaforms_Delete.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,26 +14,28 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 if (($RBAC_Response=$RBAC->userCanAccess("PM_FACTORY"))!=1) return $RBAC_Response;
 
-  require_once('classes/model/Dynaform.php');
+require_once('classes/model/Dynaform.php');
 
-  $dynaform = new dynaform();
+$dynaform = new dynaform();
 
-  if (!isset($_POST['DYN_UID'])) return;
+if (!isset($_POST['DYN_UID'])) return;
 
-  $dynaform->remove( $_POST['DYN_UID'] );
+$dynaform->remove( $_POST['DYN_UID'] );
 
-  require_once 'classes/model/Step.php';
-  $oStep = new Step();
-  $oStep->removeStep('DYNAFORM', $_POST['DYN_UID']);
+require_once 'classes/model/Step.php';
+$oStep = new Step();
+$oStep->removeStep('DYNAFORM', $_POST['DYN_UID']);
 
-?>
+require_once 'classes/model/ObjectPermission.php';
+$oOP = new ObjectPermission();
+$oOP->removeByObject('DYNAFORM', $_POST['DYN_UID']);
--- workflow/engine/methods/inputdocs/inputdocs_Delete.php
+++ workflow/engine/methods/inputdocs/inputdocs_Delete.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * inputdocs_Delete.php
- *  
+ *
  * ProcessMaker Open Source Edition
  * Copyright (C) 2004 - 2008 Colosa Inc.23
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -14,13 +14,13 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- * For more information, contact Colosa Inc, 2566 Le Jeune Rd., 
+ *
+ * For more information, contact Colosa Inc, 2566 Le Jeune Rd.,
  * Coral Gables, FL, 33134, USA, or email info@colosa.com.
- * 
+ *
  */
 try {
 	global $RBAC;
@@ -42,6 +42,9 @@ try {
   require_once 'classes/model/Step.php';
   $oStep = new Step();
   $oStep->removeStep('INPUT_DOCUMENT', $_POST['INP_DOC_UID']);
+  require_once 'classes/model/ObjectPermission.php';
+  $oOP = new ObjectPermission();
+  $oOP->removeByObject('INPUT', $_POST['INP_DOC_UID']);
 }
 catch (Exception $oException) {
 	die($oException->getMessage());
